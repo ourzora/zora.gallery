e3b43f037852871628591e74b167369cebda2d48
nickadamson <naburner@protonmail.com>
2021-06-26 16:14:31 -0700

    Fix CORS error handling

--- data/functions.js
+++ data/functions.js
@@ -1,19 +1,27 @@
 import axios from "axios"; // Requests
-import client from "data"; // GraphQL requests
-import { ZORA_MEDIA_BY_ID } from "@data/queries"; // GraphQL Queries
+import client from "./index"; // GraphQL requests
+import { ZORA_MEDIA_BY_ID } from "./queries"; // GraphQL Queries
 
 /**
  * Collect Zora media post by ID
  * @param {Number} id post number
  * @returns {Object} containing Zora media details
  */
-export const getPostByID = async (id) => {
+ export const getPostByID = async (id) => {
   // Collect post
   let post = await client.request(ZORA_MEDIA_BY_ID(id));
   post = post.media;
 
   // Collect post metadata
-  const metadata = await axios.get(post.metadataURI);
+  let metadata = {
+    data: {}
+  };
+  // Post metadata will be null if request fails
+  try {
+    metadata = await axios.get(post.metadataURI);
+  } catch (e) {
+    console.log(e)
+  }
   post.metadata = metadata.data;
 
   // Only show Zora posts
